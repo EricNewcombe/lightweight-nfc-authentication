77ad47bc98704897f85d0ad1f32812c7b7de0310
ChristianBelair <christianbelair@cmail.carleton.ca>
2021-03-29 18:14:11 -0400

    Created database

--- Server/SQL/db_init.js
+++ Server/SQL/db_init.js
@@ -2,22 +2,22 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const fs = require('fs');
 
-let db = new sqlite3.Database('nfc_auth.db', function(err){
+let db = new sqlite3.Database('../nfc_auth.db', function(err){
     if (err) {
         console.error(err.message);
     }
 
     console.log('Connected to the database.');
-    let tables = fs.readFileSync(path.join('.', 'tables.sql'));
-    let triggers = fs.readFileSync([ath.join('.', 'triggers.sql')]);
-    db.run(tables, function(err){
+    let tables = fs.readFileSync(path.join('.', 'tables.sql'), {encoding: 'utf8', flag: 'r'});
+    let triggers = fs.readFileSync(path.join('.', 'triggers.sql'), {encoding: 'utf8', flag: 'r'});
+    db.exec(tables, function(err){
         if (err) {
             console.error(err.message);
         }
 
         console.log('Created tables.');
 
-        db.run(triggers, function(err){
+        db.exec(triggers, function(err){
             if (err) {
                 console.error(err.message);
             }
