35b88e9640617090f09afa39a77e414f6acaa0dc
ericnewcombe <dukenewcombe95@gmail.com>
2021-04-15 15:52:33 -0400

    Added route to view entries in database

--- Server/routes/routeController.js
+++ Server/routes/routeController.js
@@ -1,10 +1,12 @@
 module.exports = function (app) {
     const clientAuth = require('./client-auth');
     const tagAuth = require('./tag-auth');
-    const {nfcClient, nfcTag} = require('./initialize-nfc')
+    const {nfcClient, nfcTag} = require('./initialize-nfc');
+    const viewEntries = require('./view-entries')
 
     app.use('/auth/tag', tagAuth);
     app.use('/auth/client', clientAuth);
     app.use('/initialize-nfc/client', nfcClient);
     app.use('/initialize-nfc/tag', nfcTag);
+    app.use('/view-entries', viewEntries);
 }
