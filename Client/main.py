dea644ddf7e42925047191590c1e4e7bd6d87e53
fredghostkyle <fredghostkyle@yahoo.com>
2021-04-19 13:17:05 -0400

    Basic client functions added TODO: Read and write to tag.

--- Client/main.py
+++ Client/main.py
@@ -1,25 +1,80 @@
-import nfc
-from tkinter import *
-
+#import nfc
+import requests
 
 def main():
-    r = Tk()
-    r.title('lightweight-nfc-authentication')
-    c = Canvas(master=r, width=500, height=500)
-    c.pack()
-    w = Frame(master=c)
-    w.pack()
-    button = Button(master=w, text='Stop', width=25, command=r.destroy)
-    
-    l1 = Label(master=w, text="Lightweight NFC Authentication")
-    l1.grid(row=0, column=1, columnspan=2)
-    l2 = Label(master=w, text="Sign In")
-    l2.grid(row=1, column=1)
-    button.grid(row=1, column=2)
-    #w.pack()
-    mainloop()
+
+    cid = None
+    crand = None
 
 
-    print("Hello world")
+
+
+    print("Welcome to lightweight-nfc-authentication Client")
+    while True:
+        if cid != None:
+            print("\nCurrent client id: " + str(cid))
+        printMainMenu()
+        selection = int(input("Enter a number: "))
+
+        if not isinstance(selection, int):
+            print("Not a int!")
+        else: # Save to continue!
+            if selection == 1: # New User
+                r = serverCall('/initialize-nfc/client', None).json()
+                cid = int(r["cid"])
+                crand = int(r["crand"])
+
+                print("New user ID: " + str(cid))
+                print("New user rand: " + str(crand))
+
+            elif selection == 2:
+                print("Click enter to begin reading tag")
+                result = str(input(""))
+                print("Call to tag read")
+                print("Call to server")
+                print("server result")
+                print("call to write tag new tRAND")
+            elif selection == 3:
+                print("Click enter to write to tag")
+                result = str(input(""))
+                r = serverCall('/initialize-nfc/tag', None).json()
+                tid = int(r["tid"])
+                trand = int(r["trand"])
+
+
+                print("Wrote to tag")
+
+                print("New tag ID: " + str(tid))
+                print("New tag rand: " + str(trand))
+
+            elif selection == 4:
+                r = serverCall('/view-entries', None).json()
+                print("Tags\n----------------------------------")
+                for i in r["tags"]:
+                    print(i)
+
+                print("\nClients\n----------------------------------")
+                for i in r["clients"]:
+                    print(i)
+            else:
+                print("Invalid input")
+
+
+
+
+
+def printMainMenu():
+    print("----------------------------------")
+    print("1. Create New User")
+    print("2. Authenticate Tag & User")
+    print("3. Add a new tag")
+    print("4. View All Entries (Client & Tag)")
+
+def serverCall(page, body):
+    return requests.request("GET", 'http://pass.kyleknobloch.ca:3008' + str(page), json=body)
+
+
+
+
 
 main()
